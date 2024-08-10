941193ffd545520b4dddd980c6b665a859ae4418
Tanya Nebhwani <nebhwanitanya0@gmail.com>
2024-08-11 01:47:08 +0530

    preparing for hosting frontend

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -9,7 +9,7 @@ const Signup = (props) => {
       }
     const handleSubmit=async(e)=>{
         e.preventDefault();
-        const response = await fetch('http://localhost:5000/api/auth/createUser',{
+        const response = await fetch('https://inotebook-z6r2.onrender.com/api/auth/createUser',{
             method:'POST',
           headers:{
             'Content-type':'application/json',

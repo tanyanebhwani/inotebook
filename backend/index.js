eb854c188adba95eac47c7c058b71101ef053fb2
Tanya Nebhwani <nebhwanitanya0@gmail.com>
2024-08-11 01:01:33 +0530

    cluster used in backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 var cors = require('cors');
+var dotenv = require('dotenv');
+const { default: mongoose, Mongoose } = require('mongoose');
+dotenv.config();
 mongoose.connect(
   `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.di4bo.mongodb.net/urbanMobility`, 
   {
@@ -13,19 +16,19 @@ mongoose.connect(
 .catch((err) => {
   console.error("Error connecting to database:", err);
 });
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
 
 app.use(express.json()); // To parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // To parse URL-encoded bodies
 app.use(cors());
 //Available Routes
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 app.listen(port, () => {
-  console.log(`inotebook backend listening on port ${port}`)
+  console.log(`inotebook backend listening on port ${port}`);
 })
